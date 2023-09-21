d8c8b1eeb705e189f836d8cc7edc3b2fa4c96ff6
sumin34 <s201801744@hufs.ac.kr>
2023-09-21 17:52:13 +0900

    photocontroller save 기능 추가

--- src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
+++ src/main/java/com/squarecross/photoalbum/repository/PhotoRepository.java
@@ -4,8 +4,10 @@ import com.squarecross.photoalbum.domain.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
     int countByAlbum_AlbumId(Long AlbumId);
     List<Photo> findTop4ByAlbum_AlbumIdOrderByUploadedAtDesc(Long AlbumId);
+    Optional<Photo> findByFileNameAndAlbum_AlbumId(String photoName, Long albumId);
 }
--- src/main/java/com/squarecross/photoalbum/service/PhotoService.java
+++ src/main/java/com/squarecross/photoalbum/service/PhotoService.java
@@ -6,11 +6,17 @@ import com.squarecross.photoalbum.dto.AlbumDto;
 import com.squarecross.photoalbum.dto.PhotoDto;
 import com.squarecross.photoalbum.mapper.AlbumMapper;
 import com.squarecross.photoalbum.mapper.PhotoMapper;
+import com.squarecross.photoalbum.repository.AlbumRepository;
 import com.squarecross.photoalbum.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.EntityNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +24,13 @@ import java.util.Optional;
 public class PhotoService {
     @Autowired
     private PhotoRepository photoRepository;
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    private final String original_path = Constants.PATH_PREFIX + "/photos/original";
+    private final String thumb_path = Constants.PATH_PREFIX + "/photos/thumb";
+
     public PhotoDto getPhoto(Long PhotoId){
         Optional<Photo> res = photoRepository.findById(PhotoId);
 
@@ -27,6 +40,39 @@ public class PhotoService {
         }else {
             throw new EntityNotFoundException(String.format("사진 아이디 %d로 조회되지 않았습니다", PhotoId));
         }
+    }
+
+    public PhotoDto savePhoto(MultipartFile file, Long albumId){
+        Optional<Album> res = albumRepository.findById(albumId);
+        if(res.isEmpty()){
+            throw new EntityNotFoundException("앨범이 존재하지 않습니다");
+        }
+        String fileName = file.getOriginalFilename();
+        int fileSize = (int)file.getSize();
+
+        fileName = getNextFileName(fileName, albumId);
+
+    }
+
+    private void saveFile(MultipartFile file, Long AlbumId, String fileName) throws IOException {
+        String filePath = AlbumId + "/" + fileName;
+        Files.copy(file.getInputStream(), Paths.get(original_path + "/" + filePath));
+    }
+
+    private String getNextFileName(String fileName, Long albumId){
+        String fileNameNoExt = StringUtils.stripFilenameExtension(fileName);
+        String ext = StringUtils.getFilenameExtension(fileName);
+
+        Optional<Photo> res = photoRepository.findByFileNameAndAlbum_AlbumId(fileName, albumId);
+
+        int count = 2;
+        while(res.isPresent()){
+            fileName = String.format("%s (%d).%s", fileNameNoExt, count, ext);
+            res = photoRepository.findByFileNameAndAlbum_AlbumId(fileName, albumId);
+            count++;
+        }
+
+        return fileName;
     }
 
 //    public List<PhotoDto> getPhotoList(String keyword, String sort){
